5e005e2a429f100b3102fd56f1670ccd0c927a9d
zetta <204105+zetta@users.noreply.github.com>
2023-10-03 21:01:54 +0800

    remove vcs info for tg

--- terragrunt.tf
+++ terragrunt.tf
@@ -21,27 +21,27 @@ resource "scalr_workspace" "terragrunt_dev" {
 resource "scalr_workspace" "terragrunt_stg" {
   name            = "terragrunt_stg"
   environment_id  = scalr_environment.terragrunt.id
-  vcs_provider_id = data.scalr_vcs_provider.zetta.id
+#   vcs_provider_id = data.scalr_vcs_provider.zetta.id
   auto_apply      = true
   working_directory = "terragunt/stg"
 
-  vcs_repo {
-      identifier          = "zetta/scalr-examples"
-      branch              = "main"
-      dry_runs_enabled = true
-  }
+#   vcs_repo {
+#       identifier          = "zetta/scalr-examples"
+#       branch              = "main"
+#       dry_runs_enabled = true
+#   }
 }
 
 resource "scalr_workspace" "terragrunt_prod" {
   name            = "terragrunt_prod"
   environment_id  = scalr_environment.terragrunt.id
-  vcs_provider_id = data.scalr_vcs_provider.zetta.id
+#   vcs_provider_id = data.scalr_vcs_provider.zetta.id
   auto_apply      = true
   working_directory = "terragunt/prod"
 
-  vcs_repo {
-      identifier          = "zetta/scalr-examples"
-      branch              = "main"
-      dry_runs_enabled = true
-  }
+#   vcs_repo {
+#       identifier          = "zetta/scalr-examples"
+#       branch              = "main"
+#       dry_runs_enabled = true
+#   }
 }
