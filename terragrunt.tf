d97d8b535347b9c3c93149249f8ea2901332319b
zetta <204105+zetta@users.noreply.github.com>
2023-10-03 10:35:36 +0800

    3 separate workspaces for tg

--- terragrunt.tf
+++ terragrunt.tf
@@ -4,12 +4,40 @@ resource "scalr_environment" "terragrunt" {
   account_id = var.account_id
 }
 
-resource "scalr_workspace" "terragrunt_example" {
-  name            = "terragrunt_example"
+resource "scalr_workspace" "terragrunt_dev" {
+  name            = "terragrunt_dev"
   environment_id  = scalr_environment.terragrunt.id
   vcs_provider_id = data.scalr_vcs_provider.zetta.id
   auto_apply      = true
-  working_directory = "terragunt/"
+  working_directory = "terragunt/dev"
+
+  vcs_repo {
+      identifier          = "zetta/terragrunt-examples"
+      branch              = "main"
+      dry_runs_enabled = true
+  }
+}
+
+resource "scalr_workspace" "terragrunt_stg" {
+  name            = "terragrunt_stg"
+  environment_id  = scalr_environment.terragrunt.id
+  vcs_provider_id = data.scalr_vcs_provider.zetta.id
+  auto_apply      = true
+  working_directory = "terragunt/stg"
+
+  vcs_repo {
+      identifier          = "zetta/terragrunt-examples"
+      branch              = "main"
+      dry_runs_enabled = true
+  }
+}
+
+resource "scalr_workspace" "terragrunt_prod" {
+  name            = "terragrunt_prod"
+  environment_id  = scalr_environment.terragrunt.id
+  vcs_provider_id = data.scalr_vcs_provider.zetta.id
+  auto_apply      = true
+  working_directory = "terragunt/prod"
 
   vcs_repo {
       identifier          = "zetta/terragrunt-examples"
