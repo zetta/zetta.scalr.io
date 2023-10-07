6f7aa15748e8be92fb6667ba737007c5411fa7c0
zetta <204105+zetta@users.noreply.github.com>
2023-10-07 13:29:16 +0800

    configure variables

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 terraform {
     required_providers {
         scalr = {
-            source = "registry.scalr.io/scalr/scalr"
             version= "~> 1.4.0"
         }
     }
--- terragrunt.tf
+++ terragrunt.tf
@@ -8,19 +8,37 @@ resource "scalr_workspace" "terragrunt_dev" {
   name            = "terragrunt_dev"
   environment_id  = scalr_environment.terragrunt.id
   auto_apply      = true
-#   working_directory = "terragrunt/dev"
+}
+
+resource "scalr_variable" "dev_env" {
+  key          = "env"
+  value        = "dev"
+  category     = "terraform"
+  workspace_id = scalr_workspace.terragrunt_dev.id
 }
 
 resource "scalr_workspace" "terragrunt_stg" {
   name            = "terragrunt_stg"
   environment_id  = scalr_environment.terragrunt.id
   auto_apply      = true
-#   working_directory = "terragrunt/stg"
+}
+
+resource "scalr_variable" "stg_env" {
+  key          = "env"
+  value        = "stg"
+  category     = "terraform"
+  workspace_id = scalr_workspace.terragrunt_stg.id
 }
 
 resource "scalr_workspace" "terragrunt_prod" {
   name            = "terragrunt_prod"
   environment_id  = scalr_environment.terragrunt.id
   auto_apply      = true
-#   working_directory = "terragrunt/prod"
+}
+
+resource "scalr_variable" "prod_env" {
+  key          = "env"
+  value        = "prod"
+  category     = "terraform"
+  workspace_id = scalr_workspace.terragrunt_prod.id
 }
