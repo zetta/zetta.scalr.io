efef80e8301c3866f74911c3031a3657e722a1e8
zetta <204105+zetta@users.noreply.github.com>
2023-10-07 16:48:43 +0800

    remove variables

--- terragrunt.tf
+++ terragrunt.tf
@@ -10,35 +10,14 @@ resource "scalr_workspace" "terragrunt_dev" {
   auto_apply      = true
 }
 
-resource "scalr_variable" "dev_env" {
-  key          = "environment"
-  value        = "dev"
-  category     = "terraform"
-  workspace_id = scalr_workspace.terragrunt_dev.id
-}
-
 resource "scalr_workspace" "terragrunt_stg" {
   name            = "terragrunt_stg"
   environment_id  = scalr_environment.terragrunt.id
   auto_apply      = true
 }
 
-resource "scalr_variable" "stg_env" {
-  key          = "environment"
-  value        = "stg"
-  category     = "terraform"
-  workspace_id = scalr_workspace.terragrunt_stg.id
-}
-
 resource "scalr_workspace" "terragrunt_prod" {
   name            = "terragrunt_prod"
   environment_id  = scalr_environment.terragrunt.id
   auto_apply      = true
-}
-
-resource "scalr_variable" "prod_env" {
-  key          = "environment"
-  value        = "prod"
-  category     = "terraform"
-  workspace_id = scalr_workspace.terragrunt_prod.id
 }
